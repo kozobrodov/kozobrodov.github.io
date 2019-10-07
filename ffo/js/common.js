d46cba3348eff60599d6778dd09adc5c5d33f231
Eugene Kozobrodov <eugene.kozobrodov@gmail.com>
2019-10-08 00:34:08 +0300

    new menu

--- ffo/js/common.js
+++ ffo/js/common.js
@@ -61,3 +61,12 @@ function scrollTo(selectors)
     $('html,body').animate({ scrollTop: selector_top }, 'slow');
 
 }
+
+function toggleMenu() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
