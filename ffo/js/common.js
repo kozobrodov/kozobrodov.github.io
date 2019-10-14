7c955f3d01c8006c2b48b18c64c149b380a38e75
Eugene Kozobrodov <eugene.kozobrodov@gmail.com>
2019-10-14 22:48:23 +0300

    small fixes

--- ffo/js/common.js
+++ ffo/js/common.js
@@ -6,32 +6,12 @@ jQuery(function($) {
 		
         $(document).ready( function() {
 
-            // load google map
+        // load google map
         var script = document.createElement('script');
         script.type = 'text/javascript';
         script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
             'callback=initialize';
         document.body.appendChild(script);
-
-        top_menu_height = $('.top-menu').height();
-
-        // scroll to top
-        $('#btn-back-to-top').click(function(e){
-            e.preventDefault();
-            scrollTo('#top');
-        });
-
-        // scroll to specific id when click on menu
-        $('.top-menu .navbar-nav a').click(function(e){
-            e.preventDefault(); 
-            var linkId = $(this).attr('href');
-            scrollTo(linkId);
-            if($('.navbar-toggle').is(":visible") == true){
-                $('.navbar-collapse').collapse('toggle');
-            }
-            $(this).blur();
-            return false;
-        });
 
         //gallery light box setup
         $('a.colorbox').colorbox({
@@ -50,23 +30,4 @@ function initialize() {
     };
 
     var map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
-}
-
-// scroll animation 
-function scrollTo(selectors)
-{
-
-    if(!$(selectors).size()) return;
-    var selector_top = $(selectors).offset().top - top_menu_height;
-    $('html,body').animate({ scrollTop: selector_top }, 'slow');
-
-}
-
-function toggleMenu() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
 }
